474c26fb541c23578dd176cfe172af0421a753bb
gajagajago <gajagajago@naver.com>
2021-10-14 15:31:54 +0900

    Navigation & View Prototype done (except PaceView)

---
+++ AlcoholFree WatchKit Extension/PaceView.swift
@@ -0,0 +1,20 @@
+//
+//  PaceView.swift
+//  AlcoholFree WatchKit Extension
+//
+//  Created by 유준열 on 2021/10/14.
+//
+
+import SwiftUI
+
+struct PaceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PaceView_Previews: PreviewProvider {
+    static var previews: some View {
+        PaceView()
+    }
+}
