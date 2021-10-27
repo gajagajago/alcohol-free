d1117d1db9a639fa20b87a3061bf9e58724d779b
shp7724 <shp7724@naver.com>
2021-10-27 22:07:27 +0900

    PredictionData 클래스 분리

--- AlcoholFree WatchKit Extension/MotionClassification/DrinkClassifierManager.swift
+++ AlcoholFree WatchKit Extension/MotionClassification/DrinkClassifierManager.swift
@@ -14,29 +14,10 @@ import CoreMotion
 
 
 class DrinkClassifierManager {
-    
-    static let predictionWindowSize = 70
     static let sensorUpdateInterval = 1.0 / 50.0  // 50hz
-    static let stateInLength = 400
     
+    var predictionData = PredictionData()
     let motionClassifierModel = try! MotionClassifier(configuration: .init())
-    
-    var currentIndexInPredictionWindow = 0
-    
-    let accelDataX = try! MLMultiArray(shape: [predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
-    let accelDataY = try! MLMultiArray(shape: [predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
-    let accelDataZ = try! MLMultiArray(shape: [predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
-    
-    let motionRotationRateX = try! MLMultiArray(shape: [predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
-    let motionRotationRateY = try! MLMultiArray(shape: [predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
-    let motionRotationRateZ = try! MLMultiArray(shape: [predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
-    
-    let motionQuaternionX = try! MLMultiArray(shape: [predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
-    let motionQuaternionY = try! MLMultiArray(shape: [predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
-    let motionQuaternionZ = try! MLMultiArray(shape: [predictionWindowSize] as [NSNumber], dataType: MLMultiArrayDataType.double)
-    
-    var stateOutput = try! MLMultiArray(shape:[stateInLength as NSNumber], dataType: MLMultiArrayDataType.double)
-    
     var motionManager = CMMotionManager()
     var queue = OperationQueue()
     
@@ -45,11 +26,6 @@ class DrinkClassifierManager {
         motionManager.accelerometerUpdateInterval = interval
         motionManager.gyroUpdateInterval = interval
         motionManager.deviceMotionUpdateInterval = interval
-        
-        // stateIn을 0으로 초기화 (더 좋은 방법 찾습니다)
-        for index in 0..<400 {
-            stateOutput[index] = 0
-        }
     }
     
     func startMotionUpdates() {
@@ -67,43 +43,29 @@ class DrinkClassifierManager {
     }
     
     func addAccelSampleToDataArray (motionData: CMDeviceMotion) {
-//        print("\(currentIndexInPredictionWindow): \(motionData.userAcceleration), \(motionData.rotationRate), \(motionData.attitude.quaternion)")
-        // Add the current accelerometer reading to the data array
-        accelDataX[[currentIndexInPredictionWindow] as [NSNumber]] = motionData.userAcceleration.x as NSNumber
-        accelDataY[[currentIndexInPredictionWindow] as [NSNumber]] = motionData.userAcceleration.y as NSNumber
-        accelDataZ[[currentIndexInPredictionWindow] as [NSNumber]] = motionData.userAcceleration.z as NSNumber
-        
-        motionRotationRateX[[currentIndexInPredictionWindow] as [NSNumber]] = motionData.rotationRate.x as NSNumber
-        motionRotationRateY[[currentIndexInPredictionWindow] as [NSNumber]] = motionData.rotationRate.y as NSNumber
-        motionRotationRateZ[[currentIndexInPredictionWindow] as [NSNumber]] = motionData.rotationRate.z as NSNumber
-        
-        motionQuaternionX[[currentIndexInPredictionWindow] as [NSNumber]] = motionData.attitude.quaternion.x as NSNumber
-        motionQuaternionY[[currentIndexInPredictionWindow] as [NSNumber]] = motionData.attitude.quaternion.y as NSNumber
-        motionQuaternionZ[[currentIndexInPredictionWindow] as [NSNumber]] = motionData.attitude.quaternion.z as NSNumber
-        
-        // Update the index in the prediction window data array
-        currentIndexInPredictionWindow += 1
+        predictionData.feed(motionData)
         
         // If the data array is full, call the prediction method to get a new model prediction.
-        if (currentIndexInPredictionWindow == DrinkClassifierManager.predictionWindowSize) {
+        if (predictionData.isPredictionDataReady()) {
             if let predictedActivity = performModelPrediction() {
                 
                 // Use the predicted activity here
-//                print(predictedActivity)
                 
                 // Start a new prediction window
-                currentIndexInPredictionWindow = 0
+                predictionData.resetIndexInPredictionWindow()
             }
         }
     }
     
     func performModelPrediction () -> String? {
         // Perform model prediction
-        let modelPrediction = try? motionClassifierModel.prediction(motionQuaternionX_R_: motionQuaternionX, motionQuaternionY_R_: motionQuaternionY, motionQuaternionZ_R_: motionQuaternionZ, motionRotationRateX_rad_s_: motionRotationRateX, motionRotationRateY_rad_s_: motionRotationRateY, motionRotationRateZ_rad_s_: motionRotationRateZ, motionUserAccelerationX_G_: accelDataX, motionUserAccelerationY_G_: accelDataY, motionUserAccelerationZ_G_: accelDataZ, stateIn: stateOutput)
-
+        let modelPrediction = try? motionClassifierModel.prediction(input: predictionData.getPredictionInput())
         guard let modelPrediction = modelPrediction else { return nil }
+        
         print(modelPrediction.labelProbability)
-        stateOutput = modelPrediction.stateOut
+        
+        // this stateOut becomes input for the next prediction
+        predictionData.feedback(stateOut: modelPrediction.stateOut)
 
         // Return the predicted activity - the activity with the highest probability
         return modelPrediction.label
