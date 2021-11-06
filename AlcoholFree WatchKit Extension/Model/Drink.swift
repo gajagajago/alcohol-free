ec44185629994922321d19a716f1b001fd472967
gajagajago <gajagajago@naver.com>
2021-11-06 22:42:35 +0900

    Init Drink Model

---
+++ AlcoholFree WatchKit Extension/Model/Drink.swift
@@ -0,0 +1,18 @@
+//
+//  Drink.swift
+//  AlcoholFree WatchKit Extension
+//
+//  Created by 유준열 on 2021/11/06.
+//
+
+import Foundation
+import SwiftUI
+
+struct Drink: Hashable, Codable {
+    var id: Int
+    var name: String
+    var aliases: Array<String>
+    var category: String
+    var alcoholPercent: Double
+    var volumeMl: Int
+}
